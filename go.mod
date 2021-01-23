255cba7ca09482e0509816f0f61dbf9ba9fe9e90
atrn0 <14242642+aratasato@users.noreply.github.com>
2021-01-23 18:24:13 +0900

    make input

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/atrn0/nlp-report
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "make-input":
+		err := MakeInput()
+		fmt.Println(err)
+	}
+}
+
+func MakeInput() error {
+	root := "resources/nucc"
+	files, err := ioutil.ReadDir(root)
+	if err != nil {
+		return err
+	}
+
+	contents := []byte{}
+
+	for _, file := range files {
+		content, err := ioutil.ReadFile(filepath.Join(root, file.Name()))
+		if err != nil {
+			return err
+		}
+		contents = append(contents, content...)
+	}
+
+	var out bytes.Buffer
+	for _, line := range strings.Split(string(contents), "\n") {
+		runes := []rune(line)
+		if len(runes) < 1 || runes[0] == '＠' {
+			continue
+		}
+		if len(runes) > 4 && runes[4] == '：' {
+			runes = runes[5:]
+		}
+		out.WriteString(string(runes))
+	}
+
+	if err := ioutil.WriteFile("resources/input.txt",
+		out.Bytes(), os.ModePerm); err != nil {
+		return err
+	}
+
+	return nil
+}
